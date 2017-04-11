ff59daaeee33db299df7dfebbd02183df25cf8a9
Yifan Li <yifanli5@asu.edu>
2017-04-11 14:25:57 -0700

    import pillow library

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from PIL import Image
+
+path = "./TestImages/magikarp.jpg"
+
 def main():
-    print("hello world")
+
+    im = Image.open(path)
+    pixels = list(im.getdata())
+    width, height = im.size
+    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+    print(pixels)
 
 if __name__ == "__main__": main()
 
