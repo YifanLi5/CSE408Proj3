2feb1ad7398e6ef3912be26bc1a42ee601450b29
Yifan Li <yifanli5@asu.edu>
2017-04-21 01:43:56 -0700

    calc avg pixel

--- image_processing.py
+++ image_processing.py
@@ -9,22 +9,40 @@ def _calc_avg_pixel_line_values(rgb_line, sq_length):
         avg_r_horiz += pixel.r
         avg_g_horiz += pixel.g
         avg_b_horiz += pixel.b
-        if i % sq_length == 0 and i != 0:
-            avg_pixel_line.append(rgb_tuple(r = avg_r_horiz, g = avg_b_horiz, b = avg_b_horiz))
+        if (i+1) % sq_length == 0 and i != 0:
+            avg_pixel_line.append(rgb_tuple(r = avg_r_horiz // sq_length, g = avg_b_horiz // sq_length, b = avg_b_horiz //sq_length))
+            avg_r_horiz = 0
+            avg_g_horiz = 0
+            avg_b_horiz = 0
     return avg_pixel_line
 
 def calc_avg_img_values(rgb_image, sq_length):
-    avg_image = []
+    intermediate = []
+    for line in rgb_image:
+        intermediate.append(_calc_avg_pixel_line_values(line, sq_length))
+
+    avg_img_value = []
     avg_r_vert = 0
     avg_g_vert = 0
     avg_b_vert = 0
-    for i, line in enumerate(rgb_image):
-        avg_pixel_line = _calc_avg_pixel_line_values(line, sq_length)
-        avg_r_vert
-        if i % sq_length == 0 and i != 0:
 
-        avg_image.append()
-    return avg_image
+    for col in range(len(intermediate[0])):
+        avg_pixel_line = []
+        for row in range(len(intermediate)):
+
+            rgb_pixel_intermediate = intermediate[row][col]
+            avg_r_vert += rgb_pixel_intermediate.r
+            avg_g_vert += rgb_pixel_intermediate.g
+            avg_b_vert += rgb_pixel_intermediate.b
+
+            if (row+1) % sq_length == 0 and row != 0:
+                avg_pixel_line.append(rgb_tuple(r = avg_r_vert // sq_length, g = avg_g_vert // sq_length, b = avg_b_vert // sq_length))
+                avg_r_vert = 0
+                avg_g_vert = 0
+                avg_b_vert = 0
+        avg_img_value.append(avg_pixel_line)
+
+    return avg_img_value
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image
-from sys import argv
 import color_json_handling
 import nearest_neighbor
 import emoji
+import image_processing
 from color_json_handling import rgb_tuple
 path = "./TestImages/magikarp.jpg"
 
@@ -16,21 +16,39 @@ def main():
         for j in range(width):
             pixels[i][j] = color_json_handling.rgb_tuple(*pixels[i][j])
 
-    rgb_to_color_dict = color_json_handling.parse()
-    test_tup = rgb_tuple(r=84, g=139, b=91)
+    # rgb_to_color_dict = color_json_handling.parse()
+    # test_tup = rgb_tuple(r=84, g=139, b=91)
+    #
+    # result_tup = nearest_neighbor.first_nn(test_tup, rgb_to_color_dict)
+    # color = rgb_to_color_dict[result_tup]
+    # print(result_tup)
+    # print(color)
 
-    result_tup = nearest_neighbor.first_nn(test_tup, rgb_to_color_dict)
-    color = rgb_to_color_dict[result_tup]
-    print(result_tup)
-    print(color)
-    test = emoji.emojize('Python is :thumbsup:', use_aliases=True)
+    black = rgb_tuple(r=0, g=0, b=0)
+    white = rgb_tuple(r=255, g=255, b=255)
+    row0 = [black, black, black, black]
+    row1 = [white, white, black, black]
+    row2 = [black, black, white, white]
+    row3 = [white, white, white, white]
+    test = []
+    test.append(row0)
+    test.append(row1)
+    test.append(row2)
+    test.append(row3)
 
-    f = open("file.txt","wb")
-    f.write(test.encode('utf-8'))
-    f.close()
+    result = image_processing.calc_avg_img_values(test, 2)
+
+    pretty_print(test)
+    print('\n')
+    pretty_print(result)
 
 
-
+def pretty_print(matrix):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
 
 
 if __name__ == "__main__": main()
