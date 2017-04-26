68ed3dfbd614d8049e36ad7b977bd30ad91e669d
yifanli5 <yifanli5@asu.edu>
2017-04-26 15:05:17 -0700

    predef stuff

--- emoji_json_parsing.py
+++ emoji_json_parsing.py
@@ -4,13 +4,14 @@ from collections import namedtuple
 filepath = "./emoji_list.json"
 rgb_tuple = namedtuple("rgb_tuple", ["r", "g", "b"])
 
+
 def parse():
     with open(filepath) as raw_data:
         json_data = json.load(raw_data)
 
     rgb_to_color_dict = {}
     for rgb_item in json_data:
-        tup = rgb_tuple(r = rgb_item["x"], g = rgb_item["y"], b = rgb_item["z"])
+        tup = rgb_tuple(r=rgb_item["x"], g=rgb_item["y"], b=rgb_item["z"])
         label = rgb_item["label"]
         rgb_to_color_dict[tup] = label
     return rgb_to_color_dict
---
+++ knn_all_predefined_emojis.py
@@ -0,0 +1,18 @@
+import json
+import emoji_json_parsing
+import nearest_neighbor
+
+filepath_selected_emojis = "./emojis.json"
+
+def main():
+    rgb_to_color_dict = emoji_json_parsing.parse()
+    with open(filepath_selected_emojis) as raw_data:
+        json_data = json.load(raw_data)
+
+    for rgb_item in json_data:
+        tup = emoji_json_parsing.rgb_tuple(r=rgb_item["x"], g=rgb_item["y"], b=rgb_item["z"])
+        nn = nearest_neighbor.first_nn(tup, rgb_to_color_dict)
+        print("slack_code: " + rgb_item["slack_code"] + "\tclosest_color: " + str(rgb_to_color_dict[nn]) + "\t1nn_rgb: " + str(nn))
+
+
+if __name__ == "__main__": main()
